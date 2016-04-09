19963bb9dec44e380dffa0b4e7aec3b1f5315e70
zilong.xu <zilong.xu@qunar.com>
2016-04-10 00:06:47 +0800

    sss

--- db/sql/getRoomInfo.sql
+++ db/sql/getRoomInfo.sql
@@ -1,4 +1,4 @@
-   SELECT MR.roomName, MR.roomSize, MR.devices roomDevice, B.fromTime 'from', B.toTime 'to'
+   SELECT MR.roomName, MR.roomSize, MR.devices roomDevice, B.fromTime 'from', B.toTime 'end'
      FROM MeetingRoom MR
 LEFT JOIN Booking B
        ON MR.id=B.meetingRoomId
